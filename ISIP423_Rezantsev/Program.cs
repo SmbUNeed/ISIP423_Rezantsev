b4e377a292f32eb6cf8bc1792a0054f8a20bb25f
SmbUNeed <133571383+SmbUNeed@users.noreply.github.com>
2025-10-13 10:12:03 +0300

    Реализация интерфейса

--- ISIP423_Rezantsev/Program.cs
+++ ISIP423_Rezantsev/Program.cs
@@ -241,7 +241,7 @@ namespace Prac5
         }
     }
 
-    //Основной класс системы управления университетом
+    //Основной класс системы управления университетомjfgkh,sdgblhksjdgbkjsfdbglkjhsfdglkjsdfbkgjhsdkljh;ioafasn;og
     public class UniversityManager
     {
         private List<Student> students;
