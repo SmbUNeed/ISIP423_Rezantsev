13d39d1a6457360bd3e3056c59c0434d6891aa1d
SmbUNeed <133571383+SmbUNeed@users.noreply.github.com>
2025-10-22 22:40:07 +0300

    Repairing and Compensation

--- ISIP423_Rezantsev/Program.cs
+++ ISIP423_Rezantsev/Program.cs
@@ -17,8 +17,8 @@ namespace Pr7
         private Random random = new Random();
 
         private const int FINE = 50000;
-        private int balance;
-        private int Balance { 
+        private decimal balance;
+        private decimal Balance { 
             get => balance; 
             set {
                 balance = value;
@@ -62,12 +62,51 @@ namespace Pr7
             Console.WriteLine($"{part.Name}: {part.Quantity} шт.");
         }
 
+        private void ClaimOrder(Part part)
+        {
+            if(part.Quantity <= 0)
+            {
+                if(_parts.First(q => q.Quantity > 0) != null)
+                {
+                    while (true)
+                    {
+                        part = _parts[random.Next(0, _parts.Count)];
+                        if (part.Quantity > 0) break;
+                    }
+                    RepairPart(part);
+                }
+            }
+        }
+
+        public void CompensateDamage(Part part)
+        {
+            Balance -= (CalculateReplacing(part) / 2) + (FINE * 2);
+        }
+
+        private void RepairPart(Part part)
+        {
+            if(part.Quantity <= 0) { return; }
+            part.Quantity -= 1;
+            Balance += CalculateReplacing(part);
+        }
+
+        private decimal CalculateReplacing(Part part)
+        {
+            return part.Price + part.RepairFee;
+        }
+
         private void CancelOrder()
         {
             Balance -= FINE;
             Console.Clear();
-            Console.WriteLine($"Заказ отменен, вы заплатили штраф в размере {FINE} руб.\nТекущий баланс:{Balance}");
+            Console.WriteLine($"Заказ отменен, вы оплатили штраф в размере {FINE} руб.");
+            ShowBalance();
             WaitForUser();
+        }
+
+        private void ShowBalance()
+        {
+            Console.WriteLine($"Текущий баланс:{Balance}");
         }
 
         public static void WaitForUser()
@@ -82,7 +121,7 @@ namespace Pr7
             Console.WriteLine("1. Все детали:\n2.Принять заказ\n3.Отказаться(Штраф)");
 
             bool pick = true;
-            /*while (!pick)
+            while (!pick)
             {
                 pick = false;
                 switch (Console.ReadKey().Key)
@@ -91,16 +130,16 @@ namespace Pr7
                         ShowAllPartsQuantity();
                         break;
                     case (ConsoleKey.D2):
-                        ClaimOrder();
+                        ClaimOrder(part);
                         break;
                     case (ConsoleKey.D3):
-                        CancelOrger();
+                        CancelOrder();
                         break;
                     default:
                         pick = true;
                         break;
                 }
-            }*/
+            }
         }
     }
 }
